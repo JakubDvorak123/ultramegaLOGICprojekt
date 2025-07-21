8c56b8f9cfc898f7bc7d0d4cdee7fa785fe05fe1
Samuel Poneš <ponesicek@gmail.com>
2025-07-21 12:34:02 +0200

    Connects to WIFI

--- src/main.cpp
+++ src/main.cpp
@@ -1,115 +1,119 @@
 #include "Logic.hpp"
 #include <iostream>
+#include <array>
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
 
-// Potřebné include pro práci se soubory
-#include "esp_spiffs.h"
-#include <cstdio>
+#define WIFI_SSID "ChataULesa"
+#define WIFI_PASS "ulesa529"
+#define WIFI_MAXIMUM_RETRY 5
 
-// Ve složce /data v tomto projektu jsou soubory s obrázky.
-// Obrázky musí být 10x10 pixelů v GIMPU, exportovaná jako "surová data",
-// přípona .data.
-//
-// /data je potřeba flashnout do logika pomocí příkazu "pio run -t uploadfs",
-// !!! NEFLASHUJE SE SAMO !!!
-// Ten příkaz napište do terminálu, který otevřete kliknutím na tlačítko "terminál"
-// na dolním baru, je napravo od flashovací šipky.
+static const char *TAG = "wifi_logic";
+static EventGroupHandle_t s_wifi_event_group;
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
 
-void logicMain() {
-    // Konfigurace datového oddílu
-    esp_vfs_spiffs_conf_t conf = {
-        .base_path = "/spiffs",
-        .partition_label = NULL,
-        .max_files = 5,
-        .format_if_mount_failed = true,
-    };
+static int s_retry_num = 0;
 
-    // Připojení datového oddílu na /spiffs
-    esp_err_t ret = esp_vfs_spiffs_register(&conf);
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            printf("Failed to mount or format filesystem\n");
-        } else if (ret == ESP_ERR_NOT_FOUND) {
-            printf("Failed to find SPIFFS partition\n");
+static void event_handler(void* arg, esp_event_base_t event_base,
+                         int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < WIFI_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            printf("Pokus o pripojeni k WiFi, pokus %d\n", s_retry_num);
         } else {
-            printf("Failed to initialize SPIFFS (%d)\n", ret);
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
-        return;
+        printf("Pripojeni k WiFi se nezdarilo\n");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        printf("Ziskana IP adresa:" IPSTR "\n", IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
-
-    // Otevření souboru
-    FILE* soubor = fopen("/spiffs/obrazec.data", "r");
-    if (!soubor) {
-        printf("Failed to open file: %s\n", strerror(errno));
-        return;
-    }
-
-    // Načtení dat ze souboru, pixel po pixelu.
-    uint8_t pixel[3];
-    for (int i = 0; i < display.width() * display.height(); ++i) {
-        size_t res = fread(pixel, 1, 3, soubor);
-        if (res != 3) {
-            printf("Nepodarilo se precist 3 byty %d\n", res);
-            break;
-        }
-        // Nastavení do displeje
-        display[i] = Rgb(pixel[0], pixel[1], pixel[2]);
-    }
-
-    // Zavření souboru - nezapomeňte na to!
-    fclose(soubor);
-
-    display.show(20);
 }
 
-// using namespace std;
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
 
-// #define min 1
-// #define max 255
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
-// int val = 10;
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-// bool pauza = false;
-// bool press = false;
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
 
-// typedef unsigned short int USint;
+    wifi_config_t wifi_config = {};
+    strcpy((char*)wifi_config.sta.ssid, WIFI_SSID);
+    strcpy((char*)wifi_config.sta.password, WIFI_PASS);
+    wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    wifi_config.sta.pmf_cfg.capable = true;
+    wifi_config.sta.pmf_cfg.required = false;
 
-// struct vec2
-// {
-//     USint x, y;
-// };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
-// void logicMain()
-//  {
-//     vec2 pos;
-//     pos.x = 1;
-//     pos.y = 1;
+    printf("WiFi inicializovano. Pripojuji se k SSID:%s...\n", WIFI_SSID);
 
-//     while(true)
-//     {
-//         const auto pressBUT = buttons.readAll();
+    // Cekani na pripojeni nebo chybu
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
 
-//         display.drawSquare(pos.x, pos.y, 3, Rgb(255, 0, 0));
-        
-//         display.show(val);
-//         delay(150);
-//         display.clear();
-     
-//         if(pressBUT[R_Left]) pos.x++;
-//         if(pressBUT[R_Right]) pos.x--;
+    if (bits & WIFI_CONNECTED_BIT) {
+        printf("Uspesne pripojeno k WiFi SSID:%s\n", WIFI_SSID);
+    } else if (bits & WIFI_FAIL_BIT) {
+        printf("Pripojeni k WiFi SSID:%s se nezdarilo\n", WIFI_SSID);
+    } else {
+        printf("Neocekavana udalost\n");
+    }
+}
 
-//         if(pressBUT[L_Left] && val >= min)
-//         {
-//             val--;
-//         }
-//         if(pressBUT[L_Right] && val <= max)
-//         {
-//             val++;
-//         }
-
-
-//     }
-
-//    // display.clear();
+void logicMain() {
+    printf("Spoustim WiFi program...\n");
     
-//     std::cout << "Hello world!\n";
-// }
+    // Inicializace NVS (povinne pro WiFi)
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    printf("NVS inicializovano uspesne\n");
+    
+    // Inicializace WiFi
+    wifi_init_sta();
+    
+    // Hlavni smycka programu
+    while(true) {
+        printf("WiFi program bezi...\n");
+        vTaskDelay(pdMS_TO_TICKS(5000)); // Cekani 5 sekund
+    }
+}
